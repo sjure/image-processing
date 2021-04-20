3a251941c839a09f289474aa3a0fc60a11b51260
Sjur Brekke Espedal <sjure@stud.ntnu.no>
2021-04-20 21:46:25 +0100

    removed unused files

--- omp/helpingcode.cpp
+++
@@ -1,9 +0,0 @@
-for(int n = 1; n < size; n++){
-            finalHeight = newImageHeightNode * n;
-            for (int j = 0; j < 3; j++){
-                for (int i = firstHeight; i < finalHeight; i++){
-                    rc = MPI_Send(&image[j][i][0], userWidth, MPI_DOUBLE, n, tag, MPI_COMM_WORLD);
-                }
-            }
-            firstHeight += newImageHeightNode;
-        }
